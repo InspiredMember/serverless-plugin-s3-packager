4e7a218a8bc233f524b1c4f3377f266ea79ab14d
Jeff Trudeau <jeff.trudeau@gmail.com>
2017-10-25 15:44:45 -0400

    Attempt to make more synchronous

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ let Class = require('class.extend');
 let fs = require('fs');
 let path = require('path');
 
-const deployedFiles = [];
-
 module.exports = Class.extend({
     init(serverless, opts) {
         this._serverless = serverless;
@@ -18,31 +16,19 @@ module.exports = Class.extend({
         this._cwd = this._serverless.config.servicePath;
         this._s3 = new AWS.S3();
 
+        this._deployedFiles = [];
+
         this.hooks = {
-            'before:deploy:initialize': this._beforeInitialize.bind(this),
-            'after:deploy:deploy': this._afterDeploy.bind(this),
+            'before:deploy:function:deploy': this._addFiles.bind(this),
+            'after:deploy:function:deploy': this._removeFiles.bind(this),
+            'before:deploy:createDeploymentArtifacts': this._addFiles.bind(this),
+            'after:deploy:createDeploymentArtifacts': this._removeFiles.bind(this),
         };
     },
 
-    _afterDeploy() {
-        for (let filepath of deployedFiles) {
-            fs.unlink(filepath, (error) => {
-                if (error) {
-                    throw error;
-                }
-
-                this._serverless.cli.log('Removed file: ' + filepath);
-            });
-        }
-    },
-
-    _beforeInitialize() {
-        let s3files = this._serverless.service.custom.s3files || [];
-
-        for (let s3file of s3files) {
-            let Key = s3file.Key;
-
-            this._s3.getObject({Bucket: s3file.Bucket, Key: Key}, (s3Error, data) => {
+    _addFiles() {
+        for (let s3file of this._serverless.service.custom.s3files || []) {
+            this._s3.getObject({Bucket: s3file.Bucket, Key: s3file.Key}, (s3Error, data) => {
                 if (s3Error) {
                     throw s3Error;
                 }
@@ -54,9 +40,21 @@ module.exports = Class.extend({
                         throw fsError;
                     }
 
-                    deployedFiles.push(filepath);
+                    this._deployedFiles.push(filepath);
                     this._serverless.cli.log('Wrote file: ' + filepath);
                 });
+            });
+        }
+    },
+
+    _removeFiles() {
+        for (let filepath of this._deployedFiles) {
+            fs.unlink(filepath, (error) => {
+                if (error) {
+                    throw error;
+                }
+
+                this._serverless.cli.log('Removed file: ' + filepath);
             });
         }
     },
