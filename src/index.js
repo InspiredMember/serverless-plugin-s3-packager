a588632ecb6cea43a7a63f688a8e422045506c74
Jeff Trudeau <jeff.trudeau@gmail.com>
2017-10-25 21:23:17 -0400

    Correctly use promises

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let Class = require('class.extend');
 let fs = require('fs');
 let path = require('path');
 
-let sync = require('synchronize');
+let Promise = require('bluebird');
 
 module.exports = Class.extend({
     init(serverless, opts) {
@@ -27,37 +27,79 @@ module.exports = Class.extend({
     },
 
     _afterDeploy() {
-        for (let filepath of this._deployedFiles) {
-            fs.unlink(filepath, (error) => {
-                if (error) {
-                    throw error;
-                }
-
-                this._serverless.cli.log('Removed file: ' + filepath);
+        let s3ops = this._deployedFiles.map((f) => {
+            return this.__delete(f).then(() => {
+                null;
+            }).catch((error) => {
+                throw error;
             });
-        }
+        });
+
+        return new Promise.all(s3ops).then(() => {
+            //this._serverless.cli.log('Removed files: ' + this._deployedFiles.length);
+        }).catch((error) => {
+            this._serverless.cli.log('Error removing files: ' + error);
+        });
     },
 
     _beforeInitialize() {
-      sync.fiber(() => {
-        for (let s3file of this._serverless.service.custom.s3files || []) {
-            this._s3.getObject({Bucket: s3file.Bucket, Key: s3file.Key}, (s3Error, data) => {
-                if (s3Error) {
-                    throw s3Error;
+        let s3files = this._serverless.service.custom.s3files || [];
+        let s3ops = s3files.map((f) => {
+            return this.__read({Bucket: f.Bucket, Key: f.Key}).then((data) => {
+                return this.__write(f.Dst, data);
+            }).catch((error) => {
+                throw error;
+            });
+        });
+
+        return new Promise.all(s3ops).then((results) => {
+            //this._serverless.cli.log('Added files: ' + s3files.length);
+        }).catch((error) => {
+            this._serverless.cli.log('Error adding files: ' + error);
+        });
+    },
+
+    __delete(filepath) {
+        return new Promise((resolve, reject) => {
+            fs.unlink(filepath, (error) => {
+                if (error) {
+                    reject(error);
                 }
 
-                let filepath = path.join(this._cwd, s3file.Dst);
+                this._serverless.cli.log('Removed file: ' + filepath);
 
-                fs.writeFile(filepath, data.Body, (fsError) => {
-                    if (fsError) {
-                        throw fsError;
-                    }
-
-                    this._deployedFiles.push(filepath);
-                    this._serverless.cli.log('Wrote file: ' + filepath);
-                });
+                resolve();
             });
-        }
-      });
+
+        });
+    },
+
+    __read(query) {
+        return new Promise((resolve, reject) => {
+            this._s3.getObject(query, (s3Error, data) => {
+                if (s3Error) {
+                    reject(s3Error);
+                }
+
+                resolve(data);
+            });
+        });
+    },
+
+    __write(filename, data) {
+        return new Promise((resolve, reject) => {
+            let filepath = path.join(this._cwd, filename);
+
+            fs.writeFile(filepath, data.Body, (fsError) => {
+                if (fsError) {
+                    reject(fsError);
+                }
+
+                this._deployedFiles.push(filepath);
+                this._serverless.cli.log('Added file: ' + filepath);
+
+                resolve();
+            });
+        });
     },
 });
